bc349ec6b41ee8a1a75ff3f5aa745d897a4a0186
Sangeeth Karayi <sangeethkarayi005@gmail.com>
2021-08-17 14:39:53 +0530

    X00TD: overlay: Update overlay for Bluetooth * From STOCK 438

--- device.mk
+++ device.mk
@@ -431,9 +431,11 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-     $(LOCAL_PATH)/overlay-fluid \
+    $(LOCAL_PATH)/overlay-fluid \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-system
+
 
 # RRO configuration
 TARGET_USES_RRO := true
